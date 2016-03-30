432ed89551bf455394e9cd2fdf5de6656aac65de
Greg Nokes <tsykoduk@gmail.com>
2016-03-29 17:28:42 -0700

    added output file

--- speed_to_site.rb
+++ speed_to_site.rb
@@ -16,7 +16,7 @@ end
 
 class SpeedTest < Thor
   
-  desc "Start TARGET", "Start a speed test of Target"
+  desc "Start TARGET [--runs (OPTIONAL)]", "Start a speed test of Target"
   
   method_option :runs, :type => :numeric, :default => 100, :desc => "Number of tests"
   
@@ -24,7 +24,8 @@ class SpeedTest < Thor
     target = args
     runs = options[:runs]
     a = []
-
+    File.open("out.txt", 'w') {|f|
+      f.puts "Start of run"}
 
     puts "checking speed to #{target}"
     puts "I will test the speed #{runs} times"
@@ -32,21 +33,33 @@ class SpeedTest < Thor
     runs.times do
     	b = `curl -sSLw \"%{http_code} total_time=%{time_total} time_connect=%{time_connect} time_start=%{time_starttransfer} %{url_effective}\\n\" #{target} -o /dev/null`
     	a << b.split[1].split("=")[1].to_f
+      File.open("out.txt", 'a') {|f|
+        f.puts b
+      }
     end
 
     avg_a = a.inject(0){|sum,x| sum + x } / runs * 1000
     a.sort!
-    perc99_a = a.value_from_percentile(99) * 1000
-    perc95_a = a.value_from_percentile(95) * 1000
+    perc99_a = a.value_from_percentile(99)
+    perc95_a = a.value_from_percentile(95)
     if perc99_a.nil?
       perc99_a = a.last * 1000
+    else
+      perc99_a = perc99_a * 1000
     end
 
     if perc95_a.nil?
       perc95_a = a.last * 1000
+    else
+      perc95_a = perc95_a * 1000
     end
-
-    puts "Over the #{runs} tests, the average time was #{avg_a.to_i}ms, the perc95 was #{perc95_a.to_i}ms and the perc99 was #{perc99_a.to_i}ms."
+    
+    results = "Over the #{runs} tests, the average time was #{avg_a.to_i}ms, the perc95 was #{perc95_a.to_i}ms and the perc99 was #{perc99_a.to_i}ms."
+    
+   File.open("out.txt",'a') {|f| 
+     f.puts results
+   }
+    puts results + " Detailed output is in file out.txt"
   end
 end
 
