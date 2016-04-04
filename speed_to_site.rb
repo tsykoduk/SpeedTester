3651b3297ef8acdd829d75ae24075eb6e01a7814
Greg Nokes <tsykoduk@gmail.com>
2016-04-04 14:59:14 -0700

    testing out router timing

--- speed_to_site.rb
+++ speed_to_site.rb
@@ -24,20 +24,27 @@ class SpeedTest < Thor
     target = args
     runs = options[:runs]
     a = []
+    c = []
     File.open("out.txt", 'w') {|f|
-      f.puts "Start of run"}
+      f.puts "Start of run"
+    }
 
     puts "checking speed to #{target}"
     puts "I will test the speed #{runs} times"
-
+    
     runs.times do
     	b = `curl -sSLw \"http_code=%{http_code} total_time=%{time_total} time_connect=%{time_connect} time_start=%{time_starttransfer} %{url_effective}\\n\" #{target} -o /dev/null`
     	a << b.split[1].split("=")[1].to_f
+      c << b.split[1].split("=")[1].to_f - b.split[3].split("=")[1].to_f
       File.open("out.txt", 'a') {|f|
         f.puts b
       }
+      #if we are running locally
+      #print "."
+      puts b
     end
-
+  
+    avg_c = c.inject(0){|sum,x| sum + x } / runs * 1000
     avg_a = a.inject(0){|sum,x| sum + x } / runs * 1000
     a.sort!
     perc99_a = a.value_from_percentile(99)
@@ -54,12 +61,12 @@ class SpeedTest < Thor
       perc95_a = perc95_a * 1000
     end
     
-    results = "Over the #{runs} tests, the average time was #{avg_a.to_i}ms, the perc95 was #{perc95_a.to_i}ms and the perc99 was #{perc99_a.to_i}ms."
+    results = "Over the #{runs} tests, the average time was #{avg_a.to_i}ms, the perc95 was #{perc95_a.to_i}ms and the perc99 was #{perc99_a.to_i}ms. The average time in the router was #{avg_c.to_i}"
     
    File.open("out.txt",'a') {|f| 
      f.puts results
    }
-    puts results + " Detailed output is in file out.txt"
+    puts "\r" + results + " Detailed output is in file out.txt"
   end
 end
 
